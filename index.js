c79bce9b99566e390f31aa6f0d2e5587e35ea9da
vkgiri <vkgiri11@gmail.com>
2022-11-21 14:54:14 +0530

    Foreground Objects and Code Refactoring

--- index.js
+++ index.js
@@ -9,21 +9,6 @@ for (let i = 0; i < collisions.length; i += 70) {
 	collisionsMap.push(collisions.slice(i, i + 70));
 }
 
-class Boundary {
-	static width = 48;
-	static height = 48;
-	constructor({ position }) {
-		this.position = position;
-		this.width = 48;
-		this.height = 48;
-	}
-
-	draw() {
-		c.fillStyle = 'rgba(255, 0, 0, 0)';
-		c.fillRect(this.position.x, this.position.y, this.width, this.height);
-	}
-}
-
 const boundaries = [];
 const offset = {
 	x: -740,
@@ -43,44 +28,23 @@ collisionsMap.forEach((row, row_index) => {
 });
 
 const bgImage = new Image();
-bgImage.src = './images/Pellet Town.png';
+bgImage.src = './images/Pellet-Town.png';
+
+const foregroundImage = new Image();
+foregroundImage.src = './images/Foreground-Objects.png';
 
 const playerImage = new Image();
 playerImage.src = './images/playerDown.png';
 
-class Sprite {
-	constructor({ position, image, frames = { max: 1 } }) {
-		this.position = position;
-		this.image = image;
-		this.frames = frames;
-
-		this.image.onload = () => {
-			this.width = this.image.width / this.frames.max;
-			this.height = this.image.height;
-		};
-	}
-
-	draw() {
-		c.drawImage(
-			this.image,
-			0,
-			0,
-			this.image.width / this.frames.max,
-			this.image.height,
-			this.position.x,
-			this.position.y,
-			this.image.width / this.frames.max,
-			this.image.height
-		);
-	}
-}
-
-const backGround = new Sprite({ position: { x: offset.x, y: offset.y }, image: bgImage });
 const player = new Sprite({
 	position: { x: canvas.width / 2 - 192 / 8, y: canvas.height / 2 - 68 / 4 },
 	image: playerImage,
 	frames: { max: 4 },
 });
+
+const backGround = new Sprite({ position: { x: offset.x, y: offset.y }, image: bgImage });
+
+const foreground = new Sprite({ position: { x: offset.x, y: offset.y }, image: foregroundImage });
 
 const keys = {
 	w: {
@@ -97,7 +61,7 @@ const keys = {
 	},
 };
 
-const movables = [backGround, ...boundaries];
+const movables = [backGround, ...boundaries, foreground];
 
 function detectCollisions({ rectangle1, rectangle2 }) {
 	return (
@@ -116,6 +80,7 @@ function animate() {
 		boundary.draw();
 	});
 	player.draw();
+	foreground.draw();
 
 	let canMove = true;
 	if (keys.w.pressed && lastKeyPressed === 'w') {
